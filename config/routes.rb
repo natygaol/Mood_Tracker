c51f0c9a1fddd603f06e258a382d997b3dd57b3a
nataly garcia <naty_garcioli@hotmail.com>
2021-04-08 14:45:27 -0500

    added nested routes trough user infos

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    resources :before_exercises, only: [:new, :create, :show, :index]
-  end
+  devise_for :users
   root to: 'pages#home'
+  resources :user_infos, only: [:new, :create, :show, :index] do
+    resources :before_exercises, only: [:new, :create, :show, :index] do
+    end
+  end
 end
-
 
