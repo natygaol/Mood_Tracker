a276b82621b68a04a9f814dfaf97ac5d9e7cd8c9
nataly garcia <naty_garcioli@hotmail.com>
2021-04-09 12:23:44 -0500

    controller fixed

--- app/controllers/after_exercises_controller.rb
+++ app/controllers/after_exercises_controller.rb
@@ -1,13 +1,13 @@
 class AfterExercisesController < ApplicationController
 
   def new
-    @before_exercise = AfterExercise.new
+    @after_exercise = AfterExercise.new
   end
 
   def create
-    @before_exercise = AfterExercise.new(after_exercise_params)
-    @before_exercise.user = current_user
-    @before_exercise.sent = false # consultar bien si esta ligada a user o users_info
+    @after_exercise = AfterExercise.new(after_exercise_params)
+    @after_exercise.user = current_user
+    @after_exercise.sent = false # consultar bien si esta ligada a user o users_info
     if @trip.save
       redirect_to edit_trip_path(@trip)
     else
@@ -17,7 +17,7 @@ class AfterExercisesController < ApplicationController
 
   def show
     @user = current_user
-    @before_exercise = AfterExercise.find(params[:id])
+    @after_exercise = AfterExercise.find(params[:id])
   end
 
 
