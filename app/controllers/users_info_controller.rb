98a7d06e2a6da6fb1c136a87eae91a263b6afdb9
Lucia diez <lupe.diez@gmail.com>
2021-04-09 09:19:47 -0300

    routes for user_info, before exercise, after exercise

--- app/controllers/users_info_controller.rb
+++ app/controllers/users_info_controller.rb
@@ -1,12 +1,21 @@
 class UsersInfoController < ApplicationController
 
+  
+  def index
+    @users_info = UserInfo.all
+  end
+
+  def show
+    @user_info = UserInfo.find(params[:id])
+  end
+
   def new
     @user_info = UserInfo.new
   end
 
   def create
     @user_info = UserInfo.new(params[:first_name, :last_name, :age, :gender, :address, :phone_number, :previous_illness, :doctor])
-    if  @user_info.save
+    if @user_info.save
       flash[:success] = "Object successfully created"
       redirect_to root_path
     else
@@ -14,19 +23,10 @@ class UsersInfoController < ApplicationController
       render :new
     end
   end
-
-  def show
-    @user_info = UserInfo.find(params[:id])
-  end
-
-  def index
-    @users_info = UserInfo.all
-  end
-
+  
   private
 
   def users_info_params
     params.require(:user_info).permit(:first_name, :last_name, :age, :gender, :address, :phone_number, :previous_illness,:doctor)
   end
- 
 end
