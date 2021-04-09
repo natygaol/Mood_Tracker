dfb789663832a28fedfddc50cc8bdb7a74b06b3f
nataly garcia <naty_garcioli@hotmail.com>
2021-04-09 11:15:00 -0500

    fixed action on After Controller

--- app/controllers/after_exercises_controller.rb
+++ app/controllers/after_exercises_controller.rb
@@ -5,7 +5,7 @@ class AfterExercisesController < ApplicationController
   end
 
   def create
-    @before_exercise = AfterExercise.new(before_exercise_params)
+    @before_exercise = AfterExercise.new(after_exercise_params)
     @before_exercise.user = current_user
     @before_exercise.sent = false # consultar bien si esta ligada a user o users_info
     if @trip.save
@@ -17,14 +17,14 @@ class AfterExercisesController < ApplicationController
 
   def show
     @user = current_user
-    @before_exercise = BeforeExercise.find(params[:id])
+    @before_exercise = AfterExercise.find(params[:id])
   end
 
 
   private
 
-  def before_exercise_params
-    params.require(:before_exercise).permit(:input_before, :anxiety_level, :motivation_level, :mood_level, :cardiac_frequency, :respiratory_rate , :pain)
+  def after_exercise_params
+    params.require(:after_exercise).permit(:input_before, :anxiety_level, :motivation_level, :mood_level, :cardiac_frequency, :respiratory_rate , :pain)
   end
 
 end
