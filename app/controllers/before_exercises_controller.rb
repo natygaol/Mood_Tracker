9107c68e51aca99ddb0e7a9244a457d5ab62c3ae
nataly garcia <naty_garcioli@hotmail.com>
2021-04-08 21:18:36 -0500

    added actions for controller before exercises

--- app/controllers/before_exercises_controller.rb
+++ app/controllers/before_exercises_controller.rb
@@ -1,3 +1,29 @@
 class BeforeExercisesController < ApplicationController
-  
+
+  def new
+    @before_exercise = BeforeExercises.new
+  end
+
+  def create
+    @before_exercise = BeforeExercises.new(before_exercise_params)
+    @before_exercise.user = current_user
+    @before_exercise.sent = false # consultar bien si esta ligada a user o users_info
+    if @trip.save
+      redirect_to edit_trip_path(@trip)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = current_user
+    @before_exercise = BeforeExercises.find(params[:id])
+  end
+
+
+  private
+
+  def before_exercise_params
+    params.require(:before_exercise).permit(:input_before, :anxiety_level, :motivation_level, :mood_level, :cardiac_frequency, :respiratory_rate , :pain)
+  end
 end
