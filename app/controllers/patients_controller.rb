b17cca23ffeeabc1fb9e380897465d2e4d5a3621
Lucia diez <lupe.diez@gmail.com>
2021-04-12 17:34:22 -0300

    adding show, create and new actions

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,16 +1,29 @@
 class PatientsController < ApplicationController
   def index
-    @patients = Patient.all
+    @patients = Patient.where(doctor_id: @doctor.id)
   end
 
   def show
+    @patient = Patient.find(params[:id])
   end
 
   def new
-    @patient = Patient.new(params[])
+    @patient = Patient.new(patient_params)
   end
 
   def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      redirect_to new_before_exercise_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:first_name, :last_name, :age, :address, :gender, :phone_number, :previous_illness) 
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users 
   root to: 'pages#home'
   
-    resources :patient, only:  %i[new create show index]  
-    resources :dcctor, only:  %i[new create]  
-    resources :before_exercise, only:  %i[new create]  
-    resources :after_exercise, only:  %i[new create]  
+  resources :patients, only:  %i[new create show index]  
+  resources :doctors, only:  %i[new create]  
+  resources :before_exercises, only:  %i[new create]  
+  resources :after_exercises, only:  %i[new create]  
 
 end
+
 
